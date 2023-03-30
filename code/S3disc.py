ecd575064a4a2f5db10b8eef93369974ba62afd2
John Cremona <john.cremona@gmail.com>
2023-03-30 06:36:52 -0400

    fix and improve S3disc and use it to speed up mod2 code

--- code/S3disc.py
+++ code/S3disc.py
@@ -1,19 +1,90 @@
 # Function to determine the quadratic subfield cut out by the mod-2
-# reduction of a 2-adic Galois representation, using the traces modulo
-# 4 (as suggested by John Voight)
+# reduction of an irreducible 2-adic Galois representation, either
+# using the traces modulo 4 (ideas developed with John Voight) or just
+# mod 2 information.
+
+from sage.all import ZZ, GF, PolynomialRing, Matrix, vector, prod
 
 def is_P_inert(a, P):
     F = GF(P) if P in ZZ else P.residue_field()
     return int(len(PolynomialRing(a.parent(), 'x')([-a,0,1]).roots(F))==0)
 
-def get_disc(K, S, BB, verbose=False):
-    """
-    K is a number field
-    S is a finite set of primes of K
-    BB is a "Black Box" dict with keys primes P not in S and values traces of P
+def get_disc(K, S, BB, mod4=False, verbose=False):
+    r"""Compute the discriminant (mod squares) of the splitting field of
+    an irreducible GL(2,2)-representation.
+
+    INPUT:
+
+    - K is a number field
+    - S is a finite set of primes of K, not dividing 2
+    - BB is a mod-2 "Black Box" dict with keys primes P not in S and
+      values (t,n) where the characteristic polynomial of Frobenius is
+      X^2-t*X+n
+
+    - mod4: if True then we have traces and determinants mod 4 (will
+      all be 1), otherwise we only have them mod 2.
+
+    OUTPUT:
+
+    Either D, a nonzero element of K, which is the discriminant of the
+    quadratic subfield of the splitting field of BB if the image is
+    S3, or 1 if the image is C3; or 0 if no primes with odd trace were
+    processed.
+
+    In the former case, we will have proved that the representation is
+    irreducible and found the discriminant (mod squares) of its
+    splitting field.  We can use this to find the splitting field
+    itself.  In the latter case, we expect that the representatioin is
+    reducible, but will need to do more work to prove this.
+
+    ALGORITHM:
+
+    If BB[P]=(t,n), let r = (1-t+n) = det(Frob_P-I).  There are three
+    cases, according to the conjugacy class of the image of the
+    Frobenius at P:
+
+    1. if \rho(Frob_P) = I mod 2,         then t%2 == 0 and r%4 == 0;
+    2. if \rho(Frob_P) mod 2 has order 2, then t%2 == 0 and r%2 == 0;
+    3. if \rho(Frob_P) mod 2 has order 3, then t%2 == 1 and r%2 == 1.
+
+    Note that Case 3 will happen if and only if the representation is
+    irreducible, and will occur for a set of primes of density 1/3
+    (for S3 image) or 2/3 (for C3 image).
+
+    For P which are split in the discriminant field, \rho(Frob_P) mod
+    2 has order 1 or 3, so r=0,1,3; while if P is inert then
+    \rho(Frob_P) mod 2 has order 2, so r=0 or 2.   
+
+    Hence:
+     if t%2==1 then P is split (image has order 3);
+     otherwise if r%4==2 then P is inert (image has order 2);
+     else we cannot tell (image may have order 1 or 2).
+
+    If the image is C3 then using only primes with odd trace is
+    sufficient to saturate the matrix and D=1 will be returned.  This
+    only uses the traces mod 2.
+
+    If the image is S3 with discriminant D, then primes with odd trace
+    will saturate up to codimension 1, at which point D will be
+    (represented by) the unique mod-2 vector orthogonal to every row
+    of the matrix M, which we can then find by computing the right
+    kernel of M.  This would only use traces mod 2.  However, if we
+    have mod-4 information in the BB values, then we may also using
+    primes of even trace (with the trace and determinant values mod 4)
+    we can usually saturate the matrix and then use the test vector to
+    find D; this can fail, for example with elliptic curve '162a1'
+    where the discriminant is -1 (mod squares), so we have even trace
+    for all p=3 (mod 4) but for all these we have 1+p-ap=0 (mod 4).
+
+    Hence the code must allow for M to only saturate up to codimension
+    1, even in the irreducible case.  Hence we do expect that for
+    irreducible representations we will encounter many primes with odd
+    trace, and hence prove irreducubility, unless the Black Box has
+    very little data in it.
+
     """
     # Make sure primes above 2 are all in S
-    if K is QQ:
+    if K.degree()==1:
         S2 = [] if 2 in S else [2]
     else:
         S2 = [P for P in K.primes_above(2) if P not in S]
@@ -30,20 +101,30 @@ def get_disc(K, S, BB, verbose=False):
     M = Matrix(GF(2), 0, rKS2)
     r = 0
     T = [] # test primes
-    V = [] # test vector
-
+    V = [] # test vector, entries will be v
+    reducible = True
     for P, aP in BB.items():
         if P in S:
             continue
-        aP = BB[P]
-        nP = P if K is QQ else P.norm()
-        v = 0 if aP%2==1 else 1 if (aP+nP-1)%4==0 else -1
-        if v==-1:
-            if verbose:
-                print("Skipping P={} since (aP,nP)=({},{})=({},{}) mod 4".format(P,aP,nP,aP%4,nP%4))
-            continue
+        aP, nP = BB[P]
+        v = 0
+        aPodd = aP%2==1
+        if aPodd: # then irreducible and P splits
+            reducible = False
+        else:
+            if not mod4:
+                continue
+            if (1-aP+nP)%4==2: # then P inert
+                v = 1
+            else: # 1-aP+nP = 0 (mod 4), ambiguous
+                if verbose:
+                    print("Skipping P={} since (aP,nP)=({},{})=({},{}) mod 4".format(P,aP,nP,aP%4,nP%4))
+                continue
         if verbose:
-            print("Using P={} with (aP,nP)=({},{})=({},{}) mod 4, so v={}".format(P,aP,nP,aP%4,nP%4, v))
+            if mod4:
+                print("Using P={} with (aP,nP)=({},{})=({},{}) mod 4, so v={}".format(P,aP,nP,aP%4,nP%4, v))
+            else:
+                print("Using P={} with aP={} mod 2".format(P,aP))
 
         row = vector([is_P_inert(a,P) for a in KS2])
         if verbose:
@@ -60,12 +141,26 @@ def get_disc(K, S, BB, verbose=False):
             print("*********Rank is now {}".format(r))
         T.append(P)
         V.append(v)
-        if r==rKS2:
+
+        if r == rKS2:
             break
 
     if r < rKS2:
-        print("Unable to saturate matrix using {} primes!".format(len(BB)))
-        return None
+        if verbose and mod4:
+            print("Unable to saturate matrix using {} primes!".format(len(BB)))
+        if reducible:
+            # we have not proved reducible but have no evidence of irreducible
+            return 0
+        if r==rKS2-1:
+            exponents = [int(e) for e in M.right_kernel().basis()[0]]
+            disc = prod((a for e,a in zip(exponents,KS2) if e), K(1))
+            if verbose:
+                print("Exponent vector = {}".format(exponents))
+                print("discriminant = {}".format(disc))
+            return disc
+        print("Irreducible but saturation codimension >1: need to provide aP for more primes")
+        return 0
+        
     if verbose:
         print("Final test set of primes is {}".format(T))
         print("Test vector = {}".format(V))
@@ -74,41 +169,66 @@ def get_disc(K, S, BB, verbose=False):
     exponents = [int(e) for e in Minv*V]
     if verbose:
         print("Exponent vector = {}".format(exponents))
-    disc = prod(a for e,a in zip(exponents,KS2) if e)
+    disc = prod((a for e,a in zip(exponents,KS2) if e), K(1))
 
     # check:
-    col = vector(GF(2), [is_P_inert(disc,P) for P in T])
-    if verbose:
-        print("col = {}".format(col))
-    assert col==V
+    assert V == vector(GF(2), [is_P_inert(disc,P) for P in T])
     return disc
 
 # Test functions
 
 # Create BB from an elliptic curve:
 
-def BBfromEC(E, maxn=300):
+def BBfromEC(E, maxn=100):
     maxn = ZZ(maxn) # else error over QQ
     K = E.base_field()
     N = E.conductor()
     S = (2*N).support()
-    BB = dict([(P,E.reduction(P).trace_of_frobenius()) for P in K.primes_of_bounded_norm_iter(maxn) if P not in S])
+    nm = lambda P: P
+    if K.degree()>1:
+        nm = lambda P: P.norm()
+    tr = lambda P: E.reduction(P).trace_of_frobenius()
+    BB = dict([(P,(tr(P),nm(P))) for P in K.primes_of_bounded_norm_iter(maxn) if P not in S])
     return BB
 
-## Test that the D we find is the discriminant of E up to squares:
+## Test that the D we find is the discriminant of E up to squares,
+## either with mod2 information only, or with mod4 information
 
-def testE(E, verbose=False):
+def testE(E, maxn=100, mod4=True, verbose=False):
     K = E.base_field()
     N = E.conductor()
     S = (2*N).support()
-    BB = BBfromEC(E)
-    D = get_disc(K, S, BB, verbose=verbose)
-    if D:
-        print("found D = {}".format(D))
-        discE = E.discriminant()
+    discE = E.discriminant()
+    if verbose:
+        print("Testing E = {} = {}".format(E.label(), E.ainvs()))
         print("disc(E) = {}".format(discE))
-        return (D*discE).is_square()
+    BB = BBfromEC(E, maxn)
+    D = get_disc(K, S, BB, mod4, verbose=verbose)
+    if D:
+        if verbose:
+            print("found D = {}".format(D), end="")
+        ok = (D*discE).is_square()
+        if verbose:
+            if ok:
+                print(" -- OK")
+            else:
+                print(" -- ********** wrong ************")
+        return ok
     else:
-        print("unable to determine discriminant")
-        return False
+        ok = E.two_torsion_rank()>0
+        if verbose:
+            print("unable to determine discriminant, probably reducible")
+        if verbose:
+            if ok:
+                print(" -- OK")
+            else:
+                print(" -- ********** wrong ************")
+        return ok
 
+
+def test_range(N1,N2, maxn=100, mod4=True, verbose=False):
+    from sage.all import cremona_curves
+    for E in cremona_curves(range(N1,N2+1)):
+        print(E.label(), end=": ")
+        assert testE(E, maxn, mod4, verbose)
+        print("OK")
--- code/mod2test.py
+++ code/mod2test.py
@@ -3,28 +3,48 @@
 from sage.all import QQ, prime_pi
 
 from T0T1T2 import get_T0
-from C2C3S3 import S3_extensions, C3S3_extensions
+from C2C3S3 import S3_extensions, C3_extensions, C2_extensions
+from S3disc import get_disc
 from read_modell_data import read_data, DATA_DIR
 
-# We will see tghe same set S of primes for different levels, and only want to construct the relevant cubics once so we cache them here:
+# We will see the same set S of primes for different levels, and only
+# want to construct the relevant cubics once so we cache them here.
+
+# The keys for the cache are pairs (S,D) where S is a set of primes and D is square-free, representing a discriminant mod squares.
 #
 
-# We only reset the cache if it is not set e.g. on first readong this file, so we can reread the file for a second run with non of the time-consuming# cubic searching.
+# We only reset the cache if it is not set e.g. on first readong this
+# file, so we can reread the file for a second run with non of the
+# time-consuming# cubic searching.
+
+# cubic_lists[(S,D)] is a list of monic irreducible cubics with
+#    discriminant D mod squares supported on S
 
 try:
     assert len(cubic_lists)
 except:
     cubic_lists = {}
 
-def get_cubics(S, include_C3=False):
+def get_cubics(S, D=None):
+    """Return a list of monic irreducible cubics with discriminant D mod
+    squares supported on S, either for one such D or (if D is None)
+    for all possible D.
+
+    """
     global cubic_lists
     SS = tuple(S)
-    if not SS in cubic_lists:
-        if include_C3:
-            cubic_lists[SS] = C3S3_extensions(QQ,S)
+    if D:
+        if (SS,D) in cubic_lists:
+            CC = cubic_lists[(SS,D)]
         else:
-            cubic_lists[SS] = S3_extensions(QQ,S)
-    return cubic_lists[SS]
+            cubic_lists[(SS,D)] = CC = C3_extensions(QQ,SS) if D.is_square() else S3_extensions(QQ,SS,D)
+        return CC
+    else:
+        return sum((get_cubics(S,D1) for D1 in (f.discriminant().squarefree_part() for f in C2_extensions(QQ,S))), [])
+
+# T0data[(S,cubics)] is [T0,vlist] where T0 is a list of test primes
+# and vlist a list of vectors such that a galrep is irreducible with
+# polynomial cubics[i] if its test vector is vlist[i]
 
 try:
     assert len(T0data)
@@ -33,47 +53,114 @@ except:
 
 def get_T0data(S, cubics):
     global T0data
-    SS = tuple(S)
-    if not SS in T0data:
+    key = (tuple(S), tuple(cubics))
+    if key not in T0data:
         _, T0, vlist = get_T0(QQ,S,cubics)
-        T0data[SS] = [T0, vlist]
-    return T0data[SS]
+        T0data[key] = [T0, vlist]
+    return T0data[key]
 
 # Process a single form data packet:
 
-def check1form(data, verbose=False):
+def process1form(data, proof=True, verbose=False):
+    """Input data is a dict with keys
+
+    'N': conductor
+    'k': weight
+    'd': dimension
+    'S': bad primes
+    'ap': list of ap mod ell
+    'ell': characteristic prime, always 2 here
+
+    Output is this dict with added keys
+
+    'reducible': True/False
+    'pol': cubic polynomial defining spltting field (if irreducible)
+    'gal': 'S3' or 'C3' (if irreducible)
+
+    If proof==False then we assume reducibility if all the ap given
+    (for good p) are 0 mod 2, otherwise the code wil prove this by
+    ensuring that the ap are all even for a rigorously computed test
+    test of good primes.
+
+    """
     assert data['ell'] == 2
     N = data['N']
     S = (2*N).prime_divisors()
     k = data['k']
     d = data['d']
+    aplist = data['ap']
     if verbose:
-        print("N = {}".format(N))
-        print("k = {}".format(k))
-        print("d = {}".format(d))
-        print("S = {}".format(S))
+        print(f"N = {N}")
+        print(f"k = {k}")
+        print(f"d = {d}")
+        print(f"S = {S}")
+        #print(f"ap = {aplist}")
 
-    cubics = get_cubics(S, True)
+    # First test for irreducibility by looking at the ap mod 2.  Here
+    # we have to exclude bad p since we are looking for one odd ap for
+    # a good prime p.  If we find one then the galrep is certainly
+    # irreducible, otherwise we expect it tobe reducible but will work
+    # harder to prove it.
+
+    good_primes = primes_first_n(len(aplist))
+    BB = dict([(p,(ap,1)) for  p,ap in zip(good_primes,aplist) if p not in S])
+    irreducible = any(ap%2 for ap,np in BB.values())
+    if verbose:
+        if irreducible:
+            print("Irreducible: at least one ap for good p is odd")
+        else:
+            print("Probably reducible: all known ap for good p are even")
+
+    # In the irreducible case we can compute the splitting field
+    # discriminant and then use fewer cubics
+
+    if irreducible:
+        if verbose:
+            print(" - finding discriminant...")
+        D = get_disc(QQ, S, BB)
+        if verbose:
+            print(f" - discriminant = {D}, finding cubics...")
+        cubics = get_cubics(S, D)
+        if verbose:
+            print(f" - found {len(cubics)} cubics with discriminant {D}")
+    else:
+        if not proof:
+            if verbose:
+                print("Reducible")
+                print("----------------------------------------------------------")
+            data['reducible'] = True
+            return data
+        if verbose:
+            print(f" - finding cubics...")
+        cubics = get_cubics(S)
+        if verbose:
+            print(f" - found {len(cubics)} cubics")
 
     if verbose:
-        print("candidate cubics: {}".format(cubics))
+        print(f"candidate cubics: {cubics}")
     T0, vlist = get_T0data(S,cubics)
     T0_indices = [prime_pi(p)-1 for p in T0]
     if verbose:
-        print("test prime set T0: {}".format(T0))
-
+        print(f"test prime set T0: {T0}")
+    # In order to conclude we must have the value of ap for all p in the test set T0
+        
     # Compute test vector.
-    ap = data['ap']
-    v = [int(ap[ip]) for ip in T0_indices]
+    try:
+        v = [int(aplist[ip]) for ip in T0_indices]
+        data['ok'] = True
+    except IndexError(f"not enough ap known to conclude! The test set of primes is {T0}"):
+        data['ok'] = False
+        return data
+        
     if verbose:
-        print("test vector = {}".format(v))
+        print(f"test vector = {v}")
     if v in vlist:
         i = vlist.index(v)
         data['pol'] = pol = cubics[i]
         id = pol.galois_group().id()
         data['gal'] = gal = 'S3' if id==[6,1] else 'C3'
         if verbose:
-            print("Irreducible: splitting field polynomial = {} with group {}".format(pol,gal))
+            print(f"Irreducible: splitting field polynomial = {pol} with group {gal}")
             print("----------------------------------------------------------")
         data['reducible'] = False
         return data
@@ -83,44 +170,48 @@ def check1form(data, verbose=False):
     data['reducible'] = True
     return data
 
-def display_string(data, ell=2):
+# NB Both display_string and display_all are imported by mod3test as well as being used here!
+
+def display_string(data, ell=2, concise=True):
     """Display splitting field info for one rep as a string
     """
     if data['reducible']:
-        return "{} mod {}:  reducible".format(data['label'], ell)
+        pad1 = pad2 = ""
+        if not concise:
+            pad1 = f" mod {ell}"
+            pad2 = "  "
+        return "".join([f"{data['label']}", pad1, ":", pad2, "reducible"])
 
     pol = data['pol']
-    # if pol.is_irreducible():
-    #     L = NumberField(pol,'a')
-    # else:
-    #     L = pol.splitting_field('a')
-    # if pol.base_ring() is QQ:
-    #     disc = L.discriminant().factor()
-    # else:
-    #     disc = L.relative_discriminant().factor()
-    detdisc = pol.discriminant().squarefree_part()
-    if detdisc%4 != 1:
-        detdisc*=4
-    if ell==2:
-        return "{} mod {}:  image {}, splitting field polynomial {}; determinant field discriminant {}".format(
-            data['label'], ell, data['gal'], pol, detdisc)
+    if concise:
+        if ell==2:
+            line = f"{data['label']}:{data['gal']}:{pol}"
+        else:
+            line = f"{data['label']}:{data['lingal']}:{data['octic'].factor()}:{data['gal']}:{pol.factor()}"
     else:
-        return "{} mod {}:  linear image {}, splitting field polynomial {};\n           projective image {}, splitting field polynomial {}; determinant field discriminant {}".format(
-            data['label'], ell, data['lingal'], data['octic'].factor(), data['gal'], pol.factor(), detdisc)
+        detdisc = pol.discriminant().squarefree_part()
+        if detdisc%4 != 1:
+            detdisc*=4
+        if ell==2:
+            line = f"{data['label']} mod {ell}:  image {data['gal']}, splitting field polynomial {pol}; determinant field discriminant {detdisc}"
+        else:
+            line = "\n".join([f"{data['label']} mod {ell}:  linear image {data['lingal']}, splitting field polynomial {data['octic'].factor()};",
+                              f"           projective image {data['gal']}, splitting field polynomial {pol.factor()}; determinant field discriminant {detdisc}"])
+    return line
 
-def display_all(datalist, ell=2, fname=None):
+def display_all(datalist, ell=2, concise=True, fname=None):
     """Display splitting field info for a list of irreducibles, either to
     a file (if fname is not None) or to the screen
     """
     if fname:
         with open(fname, 'a') as outfile:
             for data in datalist:
-                outfile.write(display_string(data, ell)+"\n")
+                outfile.write(display_string(data, ell, concise)+"\n")
     else:
         for data in datalist:
-            print(display_string(data, ell))
+            print(display_string(data, ell, concise))
     
-def run(fname, dir=DATA_DIR, no_repeats=False, outfilename=None, verbose=False):
+def run(fname, dir=DATA_DIR, no_repeats=False, outfilename=None, concise=True, verbose=False):
     ell = 2
     alldata = read_data(fname, ell, dir=dir)
     print("finished reading data: {} newforms".format(len(alldata)))
@@ -136,8 +227,11 @@ def run(fname, dir=DATA_DIR, no_repeats=False, outfilename=None, verbose=False):
     if outfilename:
         with open(outfilename, 'a') as outfile:
             for data in alldata:
-                res1 = check1form(data, verbose=verbose)
-                outfile.write(display_string(res1, ell)+"\n")
+                res1 = process1form(data, proof=False, verbose=verbose)
+                line = display_string(res1, ell, concise)
+                outfile.write(line+"\n")
+                if verbose:
+                    print(line)
 
                 if res1['reducible']:
                     nreds += 1
@@ -146,8 +240,9 @@ def run(fname, dir=DATA_DIR, no_repeats=False, outfilename=None, verbose=False):
                     gal_counts[res1['gal']] +=1
     else:
         for data in alldata:
-            res1 = check1form(data, verbose=verbose)
-            print(display_string(res1, ell))
+            res1 = process1form(data,  proof=False, verbose=verbose)
+            line = display_string(res1, ell, concise)
+            print(line)
 
             if res1['reducible']:
                 nreds += 1
@@ -155,7 +250,7 @@ def run(fname, dir=DATA_DIR, no_repeats=False, outfilename=None, verbose=False):
                 nirreds += 1
                 gal_counts[res1['gal']] +=1
 
-    print("{} lines written to {}".format(nreds+nirreds, outfilename))
+        print("{} lines written to {}".format(nreds+nirreds, outfilename))
     print()
     print("{} forms are reducible and {} are irreducible".format(nreds,nirreds))
     for gal in gal_counts:
